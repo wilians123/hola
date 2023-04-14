b2698f6cef455b2363f411cc6797b054a6c5347e
wilians123 <127614888+wilians123@users.noreply.github.com>
2023-04-13 23:54:49 -0600

    se agregan librerias

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "filesystem"
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
