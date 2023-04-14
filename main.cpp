1b6b91aeda23da9f1e22535e52febb1de35c6886
wilians123 <127614888+wilians123@users.noreply.github.com>
2023-04-13 23:57:55 -0600

    se agrego main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,31 @@
 
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    int cantidad;
+
+    cout << "Cuantos archivos quiere crear \n";
+    cin >> cantidad;
+    crearArchivos(cantidad);
+    cout << contarArchivosDeDirectorio();
+    cout << endl;
+    listarArchivosDeDirectorio();
+    mostrarDetalles();
+
     return 0;
 }
+
+void crearArchivos(int cantidad) {
+    string nombreArchivo="archivo";
+    int i= cantidad;
+    for(i=0; i<cantidad; i++){
+        ofstream file;
+        file.open("C:/Users/JP/Desktop/progra/" + nombreArchivo + to_string(i) +".txt");
+        file << "primera línea\n";
+        file << "segunda línea\n";
+        file << "tercera línea\n";
+        file.close();
+    }
+    cout<<"Archivos creados : " + to_string(i);
+
